7aa4f9c3354bf8391fcf453d1cc7625bfc97348f
Javi <inbox.javiergm@gmail.com>
2021-03-15 13:14:34 +0100

    Añadido fetch para Login de users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
+
+router.post("/login", function (req, res) {//ponemos nueva URL que recibe usuario y contraseña
+  console.log(req.body)
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log(email, password)
+  let dbConnection = req.app.locals.db;
+  dbConnection.collection("users")
+    .find({ email })//haga un find del usuario
+    .toArray(function (err, arrayUser) {//lo que era un objeto lo convierte en parte de un array
+      // console.log(arrayUsuario);
+      if (err !== null) {
+        res.send({ mensaje: "Ha habido un error", status: false });
+      } else {
+        if (arrayUser.length > 0) {//si encuentra un usuario en el find comparará que la contraseña que hemos metido será la misma que la del parámetro
+          if (password === arrayUser[0].password) {
+            // if (bcrypt.compareSync(password, arrayUser[0].password)) {//si puede hacerla se logueará correctamente y si no, incorrecta.
+            res.send({ mensaje: "Logueado correctamente", status: true });
+          } else {
+            res.send({ mensaje: "Contraseña incorrecta", status: false });
+          }
+        } else {
+          res.send({ mensaje: "El usuario no existe", status: false });
+        }
+      }
+    });
 });
 
 module.exports = router;
