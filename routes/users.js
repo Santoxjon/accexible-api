449f959c790b32ba3c2cd272560f0daa6cfcc3c2
Santoxjon <jonsantox@gmail.com>
2021-03-16 00:01:40 +0100

    Refactor de logs

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,9 @@ router.use(express.urlencoded({ extended: false }));
 /* GET users listing. */
 
 router.post("/login", function (req, res) {
-  console.log(req.body)
   let email = req.body.email;
   let password = req.body.password;
-  console.log(email, password)
+
   let dbConnection = req.app.locals.db;
   dbConnection.collection("users")
     .findOne(
@@ -20,8 +19,7 @@ router.post("/login", function (req, res) {
           res.json({ mensaje: "Ha habido un error", status: false });
         } else {
           if (user) {
-            // if (password === arrayUser[0].password) {
-            if (bcrypt.compareSync(password, user.password)) {//si puede hacerla se logueará correctamente y si no, incorrecta.
+            if (bcrypt.compareSync(password, user.password)) {
               res.json({ status: 0 });
             } else {
               res.json({ status: 1 });
