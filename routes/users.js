f823e2f6ebd5f0d839f110d27a851bddadc72ca0
Santoxjon <jonsantox@gmail.com>
2021-03-16 11:58:16 +0100

    cambios en el registro, login token

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ router.post("/login", function (req, res) {
         } else {
           if (user) {
             if (bcrypt.compareSync(password, user.password)) {
-              res.json({ status: 0 });
+              res.json({ status: 0, user });
             } else {
               res.json({ status: 1 });
             }
@@ -33,16 +33,12 @@ router.post("/login", function (req, res) {
 });
 
 /* POST FROM REGISTER FORM. */
-router.post('/register', (req, resp) => {
+router.post('/register', (req, res) => {
   let newUser = req.body;
-  //console.log(newUser);
-
-  let encryptPass = newUser.password;
-  //console.log("Contrasena antes de cifrado " + newUser.password);
-  let contraseniaCifrada = bcrypt.hashSync(encryptPass, 10); //  la encripta con metodo hash de bcrypt, parametro y nivel de seguridad q daremos a la contrasena
-  //console.log("contraseña cifrada:" + contraseniaCifrada); // contraseña cifrada --->     "passw" : "$2b$10$Idr63VLuTACe63fUNeg4seWNU8B/RNfUAn6fchEhO2iVBhoEvt/Qq",
-  newUser.password = contraseniaCifrada; // valor contrasena cifrada la asigno como nuevo valor del la clave de la contrasena
-  //console.log("Mi usuario con cont cifrada " + JSON.stringify(newUser)); // pinto usuario con contraseña cifrada ---> "passw" : "$2b$10$Idr63VLuTACe63fUNeg4seWNU8B/RNfUAn6fchEhO2iVBhoEvt/Qq",
+  let plainPass = newUser.password;
+  let contraseniaCifrada = bcrypt.hashSync(plainPass, 10);
+  newUser.password = contraseniaCifrada;
+  newUser.loginToken = bcrypt.hashSync(new Date().getTime() + newUser.email, 10);
 
   dbConnection = req.app.locals.db;
   dbConnection.collection('users').find({ "email": newUser.email }).toArray(function (err, datos) {
@@ -53,14 +49,14 @@ router.post('/register', (req, resp) => {
     } else {
       //console.log(datos); 
       if (datos.length > 0) {
-        resp.redirect("http://localhost:3000/register")
+        res.redirect("http://localhost:3000/register")
       } else {
-        dbConnection.collection('users').insertOne(newUser, function (err, datos) {
+        dbConnection.collection('users').insertOne(newUser, function (err) {
           if (err != null) {
             console.log(err);
             res.send("Ha habido un error: " + error);
           } else {
-            resp.redirect("http://localhost:3000/login")
+            res.redirect("http://localhost:3000/login")
           }
 
         })
