9acd32ae2e580a5a9ebf92888d640ab7c09b3736
Ssergiomc <sergiomc84@live.com>
2021-03-17 16:46:35 +0100

    actualizo la ruta para acceder a preguntas

--- routes/questions.js
+++ routes/questions.js
@@ -5,14 +5,17 @@ var router = express.Router();
  *  GET - Todas las preguntas 
 */
 router.get('/', (req, res) => {
-    req.app.locals.db.collection("questions").find().sort({ "_id": 1 }).toArray(function (err, data) {
+    dbConnection = req.app.locals.db;
+    dbConnection.collection('questions').find({}).toArray(function (err, data) {
         if (err != null) {
             console.log(err);
             res.send({ mensaje: "error: " + err });
         } else {
             res.json(data)
+            console.log(data);
         }
     });
 });
+
 
 module.exports = router;
